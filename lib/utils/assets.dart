6114654e9e8ea196b3f620d33c8125887072a77f
Vishal Patil <vr1449983@gmail.com>
2026-03-30 09:49:03 +0530

    - initial commit

--- lib/utils/assets.dart
+++ lib/utils/assets.dart
@@ -2,5 +2,6 @@ class Assets {
   static const String splashlogo = "image/splash.png";
   static const String notasks = "image/notasksyet.jpg";
   static const String garden = "image/garden.jpg";
+  static const String app_icon = "image/app_icon.png";
 
 }
